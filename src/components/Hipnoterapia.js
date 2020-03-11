a5536faa374487c4f80861e106d9e50b2f6aeb68
andredamaia <andre.jr91@gmail.com>
2020-03-11 14:49:05 -0300

    hipnoterapia pronta

--- src/components/Hipnoterapia.js
+++ src/components/Hipnoterapia.js
@@ -51,8 +51,11 @@ export default function Hipnoterapia() {
 
                         <p>	Vamos dizer que, enquanto criança, você foi constantemente negligenciado por seus pais, aquele seu pequeno desenho foi subjugado ou menosprezado, não sendo considerado bom o suficiente; que você sempre ouviu que não era inteligente porque não conseguia terminar suas tarefas de matemática sozinho (mesmo sendo muito bom em biologia). Na escola você sofreu bullying e seus professores estavam sempre te lembrando o quão lento você era, ou o quão atrasado em comparação aos seus colegas. Com este julgamento, sua caixa será preenchida apenas com crenças limitantes sobre você mesmo-</p>
 
-                        <span className="text-red">que irão criar fortes limites imaginários determinando o que 
-                        você é capaz ou não de alcançar na vida.</span>
+                        <div className="col-12 col-md-9 text-right">
+                            <span className="text-red"><i>que irão criar fortes limites imaginários determinando o que<br/>
+                            você é capaz ou não de alcançar na vida.</i></span>
+                        </div>
+                        
                     </div>
                 </div>
             </div>    
@@ -61,46 +64,61 @@ export default function Hipnoterapia() {
         <div className="hip-yellow">
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
-                        <p>Como alguém pode ter uma carreira de sucesso, ser bem pago, falar bem em público ou simplesmente confiar em si mesmo quando, dentro daquela pequena caixa, todas as impressões e sentimentos juntos mostram o quanto ele é incapaz e desmerecedor de coisas boas? A programação já foi instalada e irá ditar as experiências que fazem a pessoa "acreditar" que ela não pode ir além ou fazer melhor nisso ou naquilo… esta crença não é consciente, portanto o indivíduo apenas se classifica como azarado, infeliz ou enxerga que não teve oportunidades suficientes. 
+                    <div className="col-12 col-md-8 offset-md-2">
 
-                        Ele pode, inclusive, culpar as circunstâncias da vida ou outras pessoas, pois já que a pessoa é identificada com essa crença limitante, a mesma NÃO pode ser percebida.</p> 
+                        <p>Como alguém pode ter uma carreira de sucesso, ser bem pago, falar bem em público ou simplesmente confiar em si mesmo quando, dentro daquela pequena caixa, todas as impressões e sentimentos juntos mostram o quanto ele é incapaz e desmerecedor de coisas boas? A programação já foi instalada e irá ditar as experiências que fazem a pessoa "acreditar" que ela não pode ir além ou fazer melhor nisso ou naquilo… esta crença não é consciente, portanto o indivíduo apenas se classifica como azarado, infeliz ou enxerga que não teve oportunidades suficientes.</p>
+
+                        <span>Ele pode, inclusive, culpar as circunstâncias da vida ou outras pessoas, pois já que a pessoa é identificada com essa crença limitante, a mesma NÃO pode ser percebida.</span> 
+
                     </div>
                 </div>
             </div>    
         </div>
 
         <div className="hip-iceberg">
+            <img src={iceberg} className="img-iceberg" alt="" />
+
             <div className="container">
                 <div className="row">
                     <div className="col-12">
 
-                        <img src={iceberg} className="img-right" alt="" />
+                        
 
                         <h4>A hipnoterapia é uma fantástica porta que permite acessar “a caixa” e identificar 
                         essas crenças limitantes, substituindo-as por crenças positivas. 
                         </h4>
 
-                        <p>	Durante a sessão de hipnoterapia, você é guiado a um estado profundo de relaxamento, onde seu subconsciente pode ser acessado. Você recebe instruções para buscar memórias, sentimentos e imagens específicos.  O terapeuta então inunda sua
-                        mente com sugestões positivas que quebram e substituem antigos padrões.
-                        </p>
+                        <div className="col-12 col-md-8 offset-md-1">
+
+                            <p className="text-iceberg">	Durante a sessão de hipnoterapia, você é guiado a um estado profundo de relaxamento, onde seu subconsciente pode ser acessado. Você recebe instruções para buscar memórias, sentimentos e imagens específicos.  O terapeuta então inunda sua
+                            mente com sugestões positivas que quebram e substituem antigos padrões.
+                            </p>
+
+                        </div>
+                        
 
                     </div>
 
-                    <div className="col-12 col-md-9 red-box">
+                    <div className="col-12 col-md-8 red-box">
+
                         <p>	A hipnoterapia pode ser usada para tratar estresse, vícios em cigarros, melhorar relacionamentos, aumentar a autoestima, ajudar a ter abundância financeira por meio da autovalorização e da atitude “eu posso”. Com este tratamento você se sente mais disposto a adotar uma vida saudável, perder peso e praticar exercícios; melhora radicalmente a habilidade de falar em público e lidar com a raiva, vergonha, culpa e arrependimento – emoções pesadas que geralmente são um fardo para carregar na vida.
                         </p>
+
                     </div>
 
-                    <div className="col-12 col-md-4">
+                    <div className="col-12 col-md-5 box-left">
+
                         <h2>Como saber se<br/> tenho crenças limitantes?</h2>
 
                         <p>É muito simples, observe todos os aspectos<br/> da sua vida (seja sincero consigo mesmo):</p>
                     </div>
 
-                    <div className="col-12 col-md-8">
+
+                    <div className="col-12 col-md-7 box-right">
+
                         <p>Trabalho, relacionamentos, saúde, abundância financeira, hobbies e o quão feliz você é. Agora dê uma nota de 0 a 10 a cada um deles, sendo 0 absolutamente terrível e 10 totalmente incrível. Se você se vê em uma ou mais categorias com notas menores que 8, definitivamente tem uma crença limitante que determina suas experiências e isso não está te ajudando.
                         </p>
+
                     </div>
 
                 </div>
@@ -120,9 +138,9 @@ export default function Hipnoterapia() {
         <div className="hip-white">
             <div className="container">
                 <div className="row">
-                    <div className="col-12"> 
-                        <p>Esteja pronto para se tornar a sua melhor versão, uma vez que você muda
-                        seu mundo interior o seu mundo exterior seguirá essa transformação.</p>
+                    <div className="col-12 col-md-8 offset-md-2"> 
+                        <h2>Esteja pronto para se tornar a sua melhor versão, uma vez que você muda
+                        seu mundo interior o seu mundo exterior seguirá essa transformação.</h2>
                     </div>
                 </div>
             </div>    
