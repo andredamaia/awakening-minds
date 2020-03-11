a5682f28d50ccb0aedf2823b7b7313ee14bc5224
andredamaia <andre.jr91@gmail.com>
2020-03-11 01:17:40 -0300

    html do hip feito, falta css

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import Header from './Header';
 import Footer from './Footer';
 import Home from './components/Home';
 import Coaching from './components/Coaching';
+import Hipnoterapia from './components/Hipnoterapia';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import './global.css';
 import './Header.css';
 import './Footer.css';
+import './components/Hipnoterapia.css';
+import './Home.css';
 
 function App() {
   return(
     <>
       <Header/>
 
-      <Home />
+      {/*<Home /> */}
       {/* <Coaching /> */}
+
+      <Hipnoterapia />
 
       <Footer />
     </>
