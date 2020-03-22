47ca60fb6b3eeb522e46f7b71f209dbd4c60664d
andredamaia <andre.jr91@gmail.com>
2020-03-22 18:10:58 -0300

    kundalini feito

--- src/components/Kundalini.js
+++ src/components/Kundalini.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import chakra from '../assets/chakra.png'
+import meditation from '../assets/meditation.png'
+import rainbowsnake from '../assets/rainbow-snake.png'
+import snake from '../assets/snake.png'
+import purplekundalini from '../assets/purple-kundalini.png'
+import psicodelic from '../assets/psicodelic.png'
+import coloredkundalini from '../assets/colored-kundalini.png'
 
 
 
@@ -21,16 +27,16 @@ export default function Kundalini() {
         <div className="bg-chakra">
           <div className="container">
               <div className="row">
-                  <div className="col-12 col-md-6"> 
+                  <div className="col-12 col-md-5"> 
                     <img src={chakra} alt="" />
                   </div>
 
-                  <div className="col-12 col-md-6"> 
+                  <div className="col-12 col-md-7"> 
                     <h2>“Quando você consegue despertar a Kundalini,<br/>
                     ela começa a mover-se da sua mera potencialidade,<br/>
                     você de fato inicia um mundo totalmente diferente<br/>
                     do nosso mundo. Este é o mundo da eternidade." <br/>
-                    <span>– Carl Jung</span>
+                    <p>– Carl Jung</p>
                     </h2>
 
                   </div>
@@ -39,9 +45,14 @@ export default function Kundalini() {
         </div>
 
         <div className="bg-kundalini">
+
+          <img className="img-kundalini-1" src={meditation} alt="" />
+
+          <img className="img-kundalini-2" src={rainbowsnake} alt="" />
+
           <div className="container">
               <div className="row">
-                <div className="col-12 col-md-5"> 
+                <div className="col-12 col-md-5 offset-md-1"> 
 
                 <h3>Kundalini</h3>
 
@@ -50,12 +61,12 @@ export default function Kundalini() {
 
                 </div>
 
-                <div className="col-12 col-md-9 offset-md-3">
+                <div className="col-12 col-md-5 offset-md-3">
                   <p>Este gigante, uma vez acordado, se torna reconhecido pelo poder de sua energia, causando até mesmo uma experiência assustadora, uma vez que a compreensão do seu crescimento estimula fortemente os chakras, promovendo cura emocional.
                   </p>
                 </div>
 
-                <div className="col-12 col-md-3 offset-md-7">
+                <div className="col-12 col-md-5 offset-md-7">
                   <p>O porquê de Kundalini despertar em certas pessoas e em outras não ainda é um mistério, ao que parece cada indivíduo é escolhido independente de sua orientação espiritual, religiosa, de raça, gênero ou idade. Pessoas em todo o mundo e de diferentes origens estão "aleatoriamente" vivenciando o despertar da Kundalini de modo espontâneo, mesmo sem saber o que é; enquanto outros vêm tentando despertá-la por anos sem alcançar sucesso. </p>
                 </div>
 
@@ -64,29 +75,38 @@ export default function Kundalini() {
         </div>
 
         <div className="bg-snake">
+
+          <img className="img-snake-1" src={snake} alt="" />
+
+          <img className="img-snake-2" src={purplekundalini} alt="" />
+
+
           <div className="container">
               <div className="row">
                   <div className="col-12 text-center"> 
-                  <h3>Dito isto, é seguro acreditar que há de fato um elemento
-                  de graça e inteligência que escolhe aqueles que irão 
-                  vivenciar este fenômeno.
-                  </h3>
+                    <h3>Dito isto, é seguro acreditar que há de fato um elemento<br/>
+                    de graça e inteligência que escolhe aqueles que irão<br/> 
+                    vivenciar este fenômeno.
+                    </h3>
 
-                  <p>O objetivo da energia da Kundalini é desintegrar o Ego (eu inferior) despertando a pessoa para níveis mais altos de consciência. É a evolução da humanidade que leva a uma vida ligada à fonte, vivida de diferentes perspectivas e livre de medo ou sofrimento constantemente experienciados por indivíduos, agindo de sua consciência egoica.</p>
+                    <p className="col-12 col-md-10 offset-md-1 text-left">O objetivo da energia da Kundalini é desintegrar o Ego (eu inferior) despertando a pessoa para níveis mais altos de consciência. É a evolução da humanidade que leva a uma vida ligada à fonte, vivida de diferentes perspectivas e livre de medo ou sofrimento constantemente experienciados por indivíduos, agindo de sua consciência egoica.</p>
 
-                  <p>Ela religa o sistema nervoso e realinha o cérebro, equilibrando os lados esquerdo e direito para que trabalhem harmoniosamente. Dessa maneira, o indivíduo que passa pelo despertar da Kundalini será capaz de acessar alguns dons escondidos, como intuição, clareza mental e criatividade, além de sentidos físicos aprimorados, expansão da consciência e percepção. 
-                  </p>
+                    <p className="col-12 col-md-10 offset-md-1 text-left">Ela religa o sistema nervoso e realinha o cérebro, equilibrando os lados esquerdo e direito para que trabalhem harmoniosamente. Dessa maneira, o indivíduo que passa pelo despertar da Kundalini será capaz de acessar alguns dons escondidos, como intuição, clareza mental e criatividade, além de sentidos físicos aprimorados, expansão da consciência e percepção. 
+                    </p>
 
-                  <p className="col-12 col-md-6 offset-md-3 text-left">Deste modo, viverá uma vida guiada pelo coração e não pela mente e aprenderá a amar e cuidar de todos os seres vivos. Irá também adquirir um profundo sentimento de paz e bem-estar vindo de seu interior, o qual não é dependente ou relacionado a situações externas.</p> 
+                    <p className="col-12 col-md-6 offset-md-3 text-left">Deste modo, viverá uma vida guiada pelo coração e não pela mente e aprenderá a amar e cuidar de todos os seres vivos. Irá também adquirir um profundo sentimento de paz e bem-estar vindo de seu interior, o qual não é dependente ou relacionado a situações externas.</p> 
 
-                  <p className="col-12 col-md-6 offset-md-3 text-left">Sua vida será alinhada com seu Eu Superior e o Universo, levando a uma experiência de não dualidade, onde o indivíduo deixa de julgar e ter preconceitos para com as pessoas, situações e eventos. Operando fora do ego, a pessoa está enraizada no  momento  presente, permitindo que faça parte do fluxo da vida sem tentar controlá-lo e isto a levará à liberdade e ao amor incondicional.
-                  </p>
+                    <p className="col-12 col-md-6 offset-md-3 text-left">Sua vida será alinhada com seu Eu Superior e o Universo, levando a uma experiência de não dualidade, onde o indivíduo deixa de julgar e ter preconceitos para com as pessoas, situações e eventos. Operando fora do ego, a pessoa está enraizada no  momento  presente, permitindo que faça parte do fluxo da vida sem tentar controlá-lo e isto a levará à liberdade e ao amor incondicional.
+                    </p>
                   </div>
               </div>
           </div>    
         </div>
 
         <div className="bg-vitroviano">
+
+          <img className="colored-kundalini" src={coloredkundalini} alt="" />
+
           <div className="container">
               <div className="row">
                 <div className="col-12 col-md-9"> 
@@ -108,42 +128,36 @@ export default function Kundalini() {
                   <p>A Kundalini pode ser ativada após um Despertar Espiritual ou precedê-lo, os sintomas mais comuns da ativação são: </p>
                 </div>
 
-                <div className="col-12 col-md-4 box-yellow">
-                  <p>- Correntes de energia pelo corpo;<br/>
-                  - Sensação de extremo calor ou frio sem um fator externo específico;<br/>
-                  - Dores nas costas e de cabeça;<br/>
-                  - Pressão no peito e na cabeça, palpitações cardíacas;<br/>
-                  - Sentir-se emocionalmente sobrecarregado;<br/>
-                  - Corpo tremendo, vibrando ou sacudindo involuntariamente;<br/>
-                  - Orelhas apitando ou zunindo como abelhas;<br/>
-                  - Ouvir pessoas cantando, sons de trovões, sinos ou flautas;<br/>
-                  - Habilidades psíquicas temporárias: Clarividência, Clariaudiência, Clarisensações, Reconhecimento cognitivo;<br/>
-                  - Fadiga crônica;<br/>
-                  - Mudanças constantes de humor: chorar incontrolavelmente e pouco tempo depois rir de alegria (comumente diagnosticada incorretamente como Bipolaridade por médicos que não estão cientes do fenômeno Kundalini);<br/>
-                  - Insônia;<br/> 
-                  - Visões, sonhos vívidos;<br/>
-                  - Projeções astrais;<br/>
-                  - Medo, ansiedade, ataques de pânico, desespero e/ou depressão;<br/>
-                  - Desorientação, pensamento obsessivo, paranóia;<br/> 
-                  - Expansão da consciência, percepção realçada;<br/> 
-                  - Sentimentos de felicidade, alegria, paz, compaixão e amor;<br/>
-                  - Melhoria da visão e audição;<br/>
-                  - Devoção espiritual e curiosidade.<br/>
-                  </p>
+                <div className="col-12 col-md-4">
+                  <div className="vitroviano-box">
+                    <p>- Correntes de energia pelo corpo;<br/>
+                    - Sensação de extremo calor ou frio sem um fator externo específico;<br/>
+                    - Dores nas costas e de cabeça;<br/>
+                    - Pressão no peito e na cabeça, palpitações cardíacas;<br/>
+                    - Sentir-se emocionalmente sobrecarregado;<br/>
+                    - Corpo tremendo, vibrando ou sacudindo involuntariamente;<br/>
+                    - Orelhas apitando ou zunindo como abelhas;<br/>
+                    - Ouvir pessoas cantando, sons de trovões, sinos ou flautas;<br/>
+                    - Habilidades psíquicas temporárias: Clarividência, Clariaudiência, Clarisensações, Reconhecimento cognitivo;<br/>
+                    - Fadiga crônica;<br/>
+                    </p>
+                  </div>
                 </div>
 
-                <div className="col-12 col-md-4 box-yellow">
-                  <p>- Mudanças constantes de humor: chorar incontrolavelmente e pouco tempo depois rir de alegria (comumente diagnosticada incorretamente como Bipolaridade por médicos que não estão cientes do fenômeno Kundalini);<br/>
-                  - Insônia;<br/> 
-                  - Visões, sonhos vívidos;<br/>
-                  - Projeções astrais;<br/>
-                  - Medo, ansiedade, ataques de pânico, desespero e/ou depressão;<br/>
-                  - Desorientação, pensamento obsessivo, paranóia;<br/> 
-                  - Expansão da consciência, percepção realçada;<br/> 
-                  - Sentimentos de felicidade, alegria, paz, compaixão e amor;<br/>
-                  - Melhoria da visão e audição;<br/>
-                  - Devoção espiritual e curiosidade.<br/>
-                  </p>
+                <div className="col-12 col-md-4">
+                  <div className="vitroviano-box">
+                    <p>- Mudanças constantes de humor: chorar incontrolavelmente e pouco tempo depois rir de alegria (comumente diagnosticada incorretamente como Bipolaridade por médicos que não estão cientes do fenômeno Kundalini);<br/>
+                    - Insônia;<br/> 
+                    - Visões, sonhos vívidos;<br/>
+                    - Projeções astrais;<br/>
+                    - Medo, ansiedade, ataques de pânico, desespero e/ou depressão;<br/>
+                    - Desorientação, pensamento obsessivo, paranóia;<br/> 
+                    - Expansão da consciência, percepção realçada;<br/> 
+                    - Sentimentos de felicidade, alegria, paz, compaixão e amor;<br/>
+                    - Melhoria da visão e audição;<br/>
+                    - Devoção espiritual e curiosidade.<br/>
+                    </p>
+                  </div>
                 </div>
             
               </div>
@@ -151,9 +165,16 @@ export default function Kundalini() {
         </div>
 
         <div className="bg-psicodelic">
+
+          <img className="img-psicodelic" src={psicodelic} alt="" />
+
+          <h6>Entregue-se<br/>
+          e receba...
+          </h6>
+
           <div className="container">
               <div className="row">
-                <div className="col-12 col-md-8 offset-md-1 "> 
+                <div className="col-12 col-md-7 offset-md-3 "> 
                   <p>O que quer que você esteja vivendo neste momento, é importante se entregar e
                   confiar no processo, já que esta é uma evolução de consciência natural, e a   
                   energia trabalha com inteligência.
@@ -174,15 +195,17 @@ export default function Kundalini() {
 
                 </div>
 
-                <div className="col-12 col-md 10">
-                  <p>Confira mais artigos relacionados ao Despertar Espiritual & Kundalini 
-                  acessando o M E N U na parte superior da página.</p>
+                <div className="col-12 col-md-8 offset-md-6">
+                  <p><strong>Confira mais artigos relacionados ao Despertar Espiritual & Kundalini</strong></p>
+                  <p className="col-12 col-md-6 offset-md-2"><strong>acessando o M E N U na parte superior da página.</strong></p>
                 </div>
 
-                <div className="col-12 col-md-5">
+                <div className="col-12 col-md-5 offset-md-3">
 
-                  <p className="box-yellow">
-                  Para obter suporte durante sua ativação e agendar uma sessão online, preencha o quadro abaixo ou envie um e-mail para andressa@andressa-awakeningminds.com 
+                  <p className="psicodelic-box">
+                  Para obter suporte durante sua ativação e agendar uma 
+                  sessão online, preencha o quadro abaixo ou envie um e-mail
+                  para andressa@andressa-awakeningminds.com 
                   </p>
                 </div>
 
